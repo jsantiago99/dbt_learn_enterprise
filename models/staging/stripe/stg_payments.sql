103e5d26f6b2da4a156282a160ac982c88efb115
throwawayboi628@gmail.com <throwawayboi628@gmail.com>
2022-10-04 17:26:50 +0000

    macro practice done

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,7 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        {{ cents_to_dollars('amount') }} as amount,
+        {{ cents_to_dollars('amount',4) }} as amount,
         created as created_at
     from {{ source('stripe', 'payment')}}
 )
